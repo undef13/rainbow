7c0c0f2beca547c27b7a59a9a05df0fbe27282f0
Maksym <babiimaksym@gmail.com>
2020-11-20 18:04:57 +0200

    minor refactor in passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,15 @@
+// Dependencies
 const bcrypt = require(`bcrypt`);
+
+// User model
 const User = require(`../models/User`);
 
+// Strategies
 const LocalStrategy = require(`passport-local`).Strategy;
 const GoogleStrategy = require(`passport-google-oauth20`).Strategy;
 
 module.exports = (passport) => {
+  // Local Strategy
   passport.use(
     "local",
     new LocalStrategy(
@@ -13,22 +18,30 @@ module.exports = (passport) => {
         passwordField: "password",
       },
       async (email, password, done) => {
-        const user = await User.findOne({ email });
-        if (!user) {
-          return done(null, false, { message: "User not found" });
+        try {
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return done(null, false, { message: "User not found." });
+          }
+
+          const isValidPassword = await bcrypt.compare(password, user.password);
+
+          if (!isValidPassword) {
+            done(null, false, { message: "Wrong pasword." });
+          } else if (!user.isActive) {
+            done(null, false, { message: "Account is not activated." });
+          } else {
+            done(null, user, { message: "Logged in." });
+          }
+        } catch (error) {
+          console.error(error);
         }
-        const isValidPassword = await bcrypt.compare(password, user.password);
-        if (!isValidPassword) {
-          return done(null, false, { message: "Wrong pasword" });
-        }
-        if(!user.isActive) {
-          return done(null, false, { message: "Account is not activated." });
-        }
-        return done(null, user, { message: "Logged in." });
       }
     )
   );
 
+  // Google Strategy
   passport.use(
     "google",
     new GoogleStrategy(
@@ -39,21 +52,23 @@ module.exports = (passport) => {
         callbackURL: "http://localhost:3000/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
         const newUser = {
           email: profile.emails[0].value,
           givenName: profile.name.givenName,
           familyName: profile.name.familyName,
           displayName: profile.displayName,
-          isActive: true
+          isActive: true,
         };
-
-        let user = await User.findOne({ email: profile.emails[0].value });
-        if (user) {
-          done(null, user);
-        } else {
-          user = await User.create(newUser);
-          done(null, user);
+        try {
+          let user = await User.findOne({ email: profile.emails[0].value });
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (error) {
+          console.error(error);
         }
       }
     )
