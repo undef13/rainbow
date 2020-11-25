65c3f45e6312f13d71cbf1ef067b9e705b631d1a
Maksym <babiimaksym@gmail.com>
2020-11-25 16:04:43 +0200

    password-reset.js refactor

--- public/javascript/start-page/password-reset.js
+++ public/javascript/start-page/password-reset.js
@@ -1,66 +1,31 @@
-import { setStatus, isValidPassword } from "./helper-functions.js";
+import { checkPassword, checkPasswordRepeat } from "../common/validity-check-functions.js";
 
 /* ---------- GETTING DATA ---------- */
 const resetPasswordForm = document.getElementById("resetPasswordForm");
-const resetPasswordNewPassword = document.getElementById(
-  "resetPasswordNewPassword"
-);
-const resetPasswordNewPasswordRepeat = document.getElementById(
-  "resetPasswordNewPasswordRepeat"
-);
+const resetPasswordNewPassword = document.getElementById("resetPasswordNewPassword");
+const resetPasswordNewPasswordRepeat = document.getElementById("resetPasswordNewPasswordRepeat");
 /* ---------- END OF GETTING DATA ---------- */
 
-/* ---------- DATA VALIDATIOn ---------- */
+/* ---------- DATA VALIDATION FUNCTIONS ---------- */
+// Reset Password form
 const resetPasswordValidityCheck = () => {
   const resetPasswordNewPasswordValue = resetPasswordNewPassword.value.trim();
   const resetPasswordNewPasswordRepeatValue = resetPasswordNewPasswordRepeat.value.trim();
 
-  if (resetPasswordNewPasswordValue === "") {
-    setStatus(resetPasswordNewPassword, true, "New password can not be blank");
-  } else if (resetPasswordNewPasswordValue.length < 6) {
-    setStatus(
-      resetPasswordNewPassword,
-      true,
-      "Password can not be less then 6 symbols"
-    );
-  } else if (!isValidPassword(resetPasswordNewPasswordValue)) {
-    setStatus(
-      resetPasswordNewPassword,
-      true,
-      "Password should not contain special symbols"
-    );
-  } else {
-    setStatus(resetPasswordNewPassword, false);
-  }
-
-  if (
-    resetPasswordNewPasswordRepeatValue === "" &&
-    resetPasswordNewPasswordValue === ""
+  if(checkPassword(resetPasswordNewPasswordValue, resetPasswordNewPassword) &
+     checkPasswordRepeat(resetPasswordNewPasswordValue, resetPasswordNewPasswordRepeatValue, resetPasswordNewPasswordRepeat)
   ) {
-    setStatus(
-      resetPasswordNewPasswordRepeat,
-      true,
-      "Enter your password and then repeat it here"
-    );
-  } else if (
-    resetPasswordNewPasswordRepeatValue !== resetPasswordNewPasswordValue
-  ) {
-    setStatus(resetPasswordNewPasswordRepeat, true, "Passwords do not match");
-  } else if (resetPasswordNewPasswordRepeatValue.length < 6) {
-    setStatus(
-      resetPasswordNewPasswordRepeat,
-      true,
-      "Password can not be less then 6 symbols"
-    );
-  } else {
-    setStatus(resetPasswordNewPasswordRepeat, false);
     return true;
+  } else {
+    return false;
   }
 };
+/* ---------- END OF DATA VALIDATION FUNCTIONS ---------- */
 
+/* ---------- ACTION HANDLERS ---------- */
 resetPasswordForm.addEventListener("submit", (e) => {
   if (!resetPasswordValidityCheck()) {
     e.preventDefault();
   }
 });
-/* ---------- END OF DATA VALIDATION ---------- */
+/* ---------- END OF ACTION HANDLERS ---------- */
