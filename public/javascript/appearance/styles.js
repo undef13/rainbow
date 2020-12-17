33f6b6f6b227290053cff0708d257dd8c7005cfc
Maksym <babiimaksym@gmail.com>
2020-12-17 14:44:41 +0200

    refactor: removing jquery dependency in styles.js

--- public/javascript/appearance/styles.js
+++ public/javascript/appearance/styles.js
@@ -1,10 +1,5 @@
 // Scroll Shadow
-$(window).scroll(function () {
-  $(".navigation").toggleClass("shadow", $(this).scrollTop() > 1);
-});
-
-// Automatic textarea
-$("textarea").on("input", function() {
-	this.style.height = "1px";
-	this.style.height = (this.scrollHeight + 1) + "px";
+window.addEventListener("scroll", function () {
+	const scrollTop = window.pageYOffset;
+	document.querySelector(".navigation").classList.toggle("shadow", scrollTop > 1);
 });
--- public/javascript/profile/profile.js
+++ public/javascript/profile/profile.js
@@ -1,5 +1,14 @@
 // "use strict";
 
+// Automatic textarea
+const elements = document.querySelectorAll("textarea");
+for(let element of elements) {
+	element.addEventListener("input", () => {
+		element.style.height = "1px";
+		element.style.height = (element.scrollHeight + 1) + "px";
+	})
+}
+
 // /* --------- ADD POST --------- */
 // // const postTextArea = document.getElementById("postTextArea");
 // // const postSelectVisibility = document.getElementById("selectPostVisibility");
